72f35c77ff748bf9d00bcdd74aa9e5e4eee9fb1e
Claire Griffin <clairetg@stanford.edu>
2017-03-22 15:50:38 -0700

    changes for swarm-swagger and arquillian

--- src/test/java/org/lockss/laaws/mdq/api/impl/GetAuAuidTest.java
+++ src/test/java/org/lockss/laaws/mdq/api/impl/GetAuAuidTest.java
@@ -27,102 +27,133 @@
  */
 package org.lockss.laaws.mdq.api.impl;
 
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.Matchers.is;
-//import io.restassured.builder.RequestSpecBuilder;
-//import io.restassured.http.ContentType;
-import java.io.File;
-import java.net.URL;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-//import org.lockss.rs.auth.AccessControlFilter;
+import org.lockss.rs.auth.AccessControlFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(Arquillian.class)
 public class GetAuAuidTest {
   @ArquillianResource
   private URL url;
+  private static Path tempDir;
+  private static String configfiles[] = {"common.xml", "lockss.txt", "lockss.opt"};
+
+
+  private static String initConfigFiles() throws IOException {
+    StringWriter sw = new StringWriter();
+    // copy the files
+    tempDir = Files.createTempDirectory("laawsmdq");
+    System.out.println("Moving config files to tempDir: " + tempDir);
+    for(String config : configfiles) {
+      Path src = FileSystems.getDefault().getPath(config);
+      Path dst = tempDir.resolve(src.getFileName());
+      Files.copy(src,dst, StandardCopyOption.COPY_ATTRIBUTES);
+      sw.append(" ").append("-p ");
+      sw.append(dst.toString());
+    }
+    // make sure we have the test ejb-jar file pointing to the correct values
+    Path template = FileSystems.getDefault().getPath("template-ejb-jar.xml");
+    Charset charset = StandardCharsets.UTF_8;
+    String content = new String(Files.readAllBytes(template), charset);
+    content = content.replaceAll("CONF_FILES_PLACEHOLDER", sw.toString());
+    Path ejb_test = tempDir.resolve("ejb-jar.xml");
+    Files.write(ejb_test, content.getBytes(charset));
+    return ejb_test.toString();
+  }
+
 
   @Deployment(testable = false)
-  public static WebArchive createDeployment() throws Exception {
-    String cwd = new File(".").getCanonicalPath();
-    System.out.println("cwd = " + cwd);
-    WebArchive wa = ShrinkWrap.create(WebArchive.class);
+  public static WebArchive createDeployment() {
 
-//    wa.addAsLibrary(new File("lockss-daemon/lib/lockss.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/xercesImpl-2.7.1.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/commons-collections-3.2.2.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/jakarta-oro-2.0.8.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/commons-lang3-3.4.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/derby.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/commons-httpclient-3.0-rc4.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/xstream-1.1.3.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/jetty-5.1.5L.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/commons-io-2.4.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/gettext-commons-0.9.6.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/htmlparser-1.6p.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/derbyclient.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/postgresql-jdbc4-9.1.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/mysql-connector-java-5.1.29-bin.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/castor-0.9.4.1-xml.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/commons-beanutils-1.9.2.jar"));
-//    wa.addAsLibrary(new File("lockss-daemon/lib/derbynet.jar"));
-//    wa.addPackages(true, "org.lockss.laaws.mdq.api"
-//	,"org.lockss.laaws.mdq.model"
-//	,"org.lockss.laaws.mdq.server"
-//	,"org.lockss.laaws.mdq.ejb"
-//	)
-//    .addClass(GetAuAuidTest.class)
-//    .addAsWebInfResource(new File("src/main/webapp/WEB-INF/ejb-jar.xml"))
-//    //.addAsWebInfResource(new File("src/main/webapp/WEB-INF/jboss-deployment-structure.xml"))
-//    //.addAsWebInfResource(new File("src/main/webapp/WEB-INF/web.xml"))
-//    //.addAsManifestResource(new File("src/main/webapp/META-INF/MANIFEST.MF"))
-//    .addAsWebInfResource(new File("common.xml"), "classes/common.xml")
-//    .addAsWebInfResource(new File("lockss.txt"), "classes/lockss.txt")
-//    .addAsWebInfResource(new File("lockss.opt"), "classes/lockss.opt")
-//    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-//
-//    System.out.println("wa = " + wa.toString(true));
-    return wa;
+    String ejb_file = "src/main/webapp/WEB-INF/ejb-jar.xml";
+    try {
+      ejb_file = initConfigFiles();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+
+    // Import Maven runtime dependencies
+    File[] files = Maven.resolver()
+        .loadPomFromFile("pom.xml")
+        .importRuntimeDependencies()
+        .resolve()
+        .withTransitivity()
+        .asFile();
+
+    // Create deploy file
+    WebArchive war = ShrinkWrap.create(WebArchive.class)
+        .addPackages(true,"org.lockss.laaws.mdq",
+                  "org.lockss.laaws.mdq.model",
+            "org.lockss.laaws.mdq.server",
+            "org.lockss.laaws.mdq.api",
+            "org.lockss.laaws.mdq.ebj")
+        .addAsWebInfResource(new File(ejb_file))
+        .addAsWebInfResource(new File("common.xml"), "common.xml")
+        .addAsWebInfResource(new File("lockss.txt"), "lockss.txt")
+        .addAsWebInfResource(new File("lockss.opt"), "lockss.opt")
+        .addAsLibraries(files)
+        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+    // Show the deploy structure
+    //System.out.println(war.toString(true));
+    return war;
   }
 
   @Test
   public void test1() throws Exception {
     System.out.println("GetAuAuidTest.test1() invoked");
-//    RequestSpecBuilder reqSpecBuilder = new RequestSpecBuilder();
-//    reqSpecBuilder.setBaseUri(url.toURI());
-//    given(reqSpecBuilder.build())
-//            .when().get("/aus/noAuth")
-//            .then()
-//            .contentType(ContentType.JSON)
-//            .statusCode(401)
-//            .body("message", is(AccessControlFilter.noAuthorizationHeader));
-//
-//    reqSpecBuilder = new RequestSpecBuilder();
-//    reqSpecBuilder.setBaseUri(url.toURI());
-//    given(reqSpecBuilder.build())
-//    .auth().preemptive().basic("username", "password")
-//    .when().get("/aus/badAuth")
-//    .then()
-//    .contentType(ContentType.JSON)
-//    .statusCode(401)
-//    .body("message", is(AccessControlFilter.badCredentials));
-//
-//    reqSpecBuilder = new RequestSpecBuilder();
-//    reqSpecBuilder.setBaseUri(url.toURI());
-//    given(reqSpecBuilder.build())
-//    .auth().preemptive().basic("lockss-u", "lockss-p")
-//    .when().get("/aus/noAU")
-//    .then()
-//    .contentType(ContentType.JSON)
-//    .statusCode(404)
-//    .body("message", is(AccessControlFilter.badCredentials));
 
-    System.out.println("GetAuAuidTest.test1() done");
+    RequestSpecBuilder reqSpecBuilder = new RequestSpecBuilder();
+    reqSpecBuilder.setBaseUri(url.toURI());
+    given(reqSpecBuilder.build())
+            .when().get("/aus/noAuth")
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(401)
+            .body("message", is(AccessControlFilter.noAuthorizationHeader));
+
+    reqSpecBuilder = new RequestSpecBuilder();
+    reqSpecBuilder.setBaseUri(url.toURI());
+    given(reqSpecBuilder.build())
+    .auth().preemptive().basic("username", "password")
+    .when().get("/aus/badAuth")
+    .then()
+    .contentType(ContentType.JSON)
+    .statusCode(401)
+    .body("message", is(AccessControlFilter.badCredentials));
+/*
+    This test fails because 'noAu' is throwing before the test completes.
+    reqSpecBuilder = new RequestSpecBuilder();
+    reqSpecBuilder.setBaseUri(url.toURI());
+    given(reqSpecBuilder.build())
+    .auth().preemptive().basic("lockss-u", "lockss-p")
+    .when().get("/aus/noAU")
+    .then()
+    .contentType(ContentType.JSON)
+    .statusCode(404)
+    .body("message", is(AccessControlFilter.badCredentials));
+
+  */
+  System.out.println("GetAuAuidTest.test1() done");
   }
 }
