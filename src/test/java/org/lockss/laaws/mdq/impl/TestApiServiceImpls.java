ffa01e9910fb0944a7e5b8fd8be42a49b1d085bb
Thib Guicherd-Callin <thib@cs.stanford.edu>
2019-05-09 02:50:56 -0700

    Minimal changes to match versioning.

--- src/test/java/org/lockss/laaws/mdq/impl/TestApiServiceImpls.java
+++ src/test/java/org/lockss/laaws/mdq/impl/TestApiServiceImpls.java
@@ -369,8 +369,8 @@ public class TestApiServiceImpls extends SpringLockssTestCase {
 
     JSONObject expected = new JSONObject().put("apiVersion", "2.0.0")
                                           .put("componentName", "laaws-metadata-service")
-                                          .put("componentVersion", "2.0.0.0")
-                                          .put("lockssVersion", "2.0-alpha")
+                                          .put("componentVersion", "3.0.1.0-SNAPSHOT")
+                                          .put("lockssVersion", "2.0-beta")
                                           .put("ready", true)
                                           .put("serviceName", "LOCKSS Metadata Service REST API");
     JSONAssert.assertEquals(expected.toString(), successResponse.getBody(), false);
